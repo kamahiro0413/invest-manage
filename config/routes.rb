fed46b167348e6005e6c17685a01bf2649fdb3c5
kamahiro0413 <hiroki0413ga@yahoo.co.jp>
2021-01-16 17:40:13 +0900

    テーブル作成(investment)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'investments/index'
-  root to: "investments#index"
+  root to: "groups#index"
   resources :groups, only: [:new,:create]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_15_221218) do
+ActiveRecord::Schema.define(version: 2021_01_16_081442) do
 
   create_table "group_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "group_id"
@@ -25,6 +25,21 @@ ActiveRecord::Schema.define(version: 2021_01_15_221218) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "investments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.date "date", null: false
+    t.integer "money", null: false
+    t.string "content"
+    t.integer "type", null: false
+    t.integer "account", null: false
+    t.integer "period"
+    t.bigint "user_id"
+    t.bigint "group_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["group_id"], name: "index_investments_on_group_id"
+    t.index ["user_id"], name: "index_investments_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -42,4 +57,6 @@ ActiveRecord::Schema.define(version: 2021_01_15_221218) do
 
   add_foreign_key "group_users", "groups"
   add_foreign_key "group_users", "users"
+  add_foreign_key "investments", "groups"
+  add_foreign_key "investments", "users"
 end
