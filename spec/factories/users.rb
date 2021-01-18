ffe2187ffe4c3c3430cb2ef2ccdf3d45cb35e3bd
kamahiro0413 <hiroki0413ga@yahoo.co.jp>
2021-01-18 12:40:45 +0900

    ユーザー新規登録のテスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {"testuser"}
+    email                 {"test@email.com"}
+    password              {"123456"}
+    password_confirmation {password}
+  end
+end
