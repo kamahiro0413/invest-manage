8de40995769f80c9401b352b60743c2a32958290
kamahiro0413 <hiroki0413ga@yahoo.co.jp>
2021-01-25 18:22:56 +0900

    chart.jsの導入

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,7 @@
 class GroupsController < ApplicationController
+  before_action :set_total, only: :show
+  before_action :set_category_sum, only: :show
+
   def index
     # @user = User.find(params[:id])
     # @group = Group.includes(:user)
@@ -18,9 +21,29 @@ class GroupsController < ApplicationController
     end
   end
 
+  def show
+    @group = Group.find(params[:id])
+    @investment = Investment.where(group_id: @group)
+  end
+
   private
 
   def group_params
     params.require(:group).permit(:name, user_ids: [])
   end
+
+  def set_total
+    @group = Group.find(params[:id])
+    @investments = @group.investments
+    @total = Investment.where(group_id: @group).sum(:money)
+  end
+
+  def set_category_sum
+    @category2 = @group.investments.where(category_id: "2").pluck(:money).sum
+    @category3 = @group.investments.where(category_id: "3").pluck(:money).sum
+    @category4 = @group.investments.where(category_id: "4").pluck(:money).sum
+    @category5 = @group.investments.where(category_id: "5").pluck(:money).sum
+    # @category4 = @group.investments.where(category_id: "4").pluck(:money).sum
+    # @category4 = @group.investments.where(category_id: "4").pluck(:money).sum
+  end  
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -5,13 +5,10 @@ class Category < ActiveHash::Base
     { id: 3,  name: '国内株式' },
     { id: 4,  name: '外国株式' },
     { id: 5,  name: '投資信託' },
-    { id: 6,  name: 'NISA' },
-    { id: 7,  name: 'iDeCo' },
-    { id: 8,  name: '債券' },
-    { id: 9,  name: 'コモディティ' },
-    { id: 10, name: '暗号資産' },
-    { id: 11, name: '外貨預金' },
-    { id: 12, name: 'その他' }
+    { id: 6,  name: '債券' },
+    { id: 7,  name: 'コモディティ' },
+    { id: 8,  name: '暗号資産' },
+    { id: 9,  name: 'その他' }
   ]
 
   include ActiveHash::Associations
