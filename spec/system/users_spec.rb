1df38e224a310dc68a059dcdfdf642bc60c7baaf
kamahiro0413 <hiroki0413ga@yahoo.co.jp>
2021-01-31 20:28:43 +0900

    結合テスト(ログイン)

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -52,3 +52,52 @@ RSpec.describe 'ユーザーの新規登録', type: :system do
     end
   end
 end
+
+RSpec.describe 'ログイン', type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+
+  context 'ログインができるとき' do
+
+    it '保存されているユーザーの情報と合致すればログインができる' do
+      # トップページに移動
+      visit root_path
+      # トップページにログインボタンがあることを確認
+      expect(page).to have_content('ログイン')
+      # ログインページへ移動
+      visit new_user_session_path
+      # 正しいユーザー情報を入力
+      fill_in 'user_email', with: @user.email
+      fill_in 'user_password', with: @user.password
+      # ログインボタンを押す
+      find('input[name="commit"]').click
+      # トップページに移動することを確認
+      expect(current_path).to eq root_path
+      # ログアウトボタンがあることを確認
+      expect(page).to have_content('ログアウト')
+      # 新規登録ボタンや、ログインボタンが表示されていないことを確認
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
+    end
+  end
+
+  context 'ログインができないとき' do
+
+    it '保存されているユーザーの情報と合致しないとログインができない' do
+      # トップページに移動
+      visit root_path
+      # トップページにログインボタンがあることを確認
+      expect(page).to have_content('ログイン')
+      # ログインページへ移動
+      visit new_user_session_path
+      # ユーザー情報を入力
+      fill_in 'user_email', with: ""
+      fill_in 'user_password', with: ""
+      # ログインボタンを押す
+      find('input[name="commit"]').click
+      # ログインページへ戻されることを確認する
+      expect(current_path).to eq new_user_session_path
+    end
+  end
+end
