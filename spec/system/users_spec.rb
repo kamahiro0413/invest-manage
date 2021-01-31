1cd1ffd664658377aaee0cd4b1b9d83492b819e4
kamahiro0413 <hiroki0413ga@yahoo.co.jp>
2021-01-31 19:02:40 +0900

    結合テスト(ユーザー新規登録)

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe 'ユーザーの新規登録', type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  context '新規登録できるとき' do
+
+    it '正しい情報を入力すれば新規登録が完了しトップページに移動する' do
+      # トップページに移動
+      visit root_path
+      # トップページに新規登録ボタンがあることを確認
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動
+      visit new_user_registration_path
+      # ユーザー情報を入力
+      fill_in 'user_nickname', with: @user.nickname
+      fill_in 'user_email', with: @user.email
+      fill_in 'user_password', with: @user.password
+      fill_in 'user_password_confirmation', with: @user.password_confirmation
+      # 登録ボタンを押すとユーザーモデルのカウントが1上がることを確認
+      expect{find('input[name="commit"]').click}.to change { User.count }.by(1)
+      # トップページへ移動
+      expect(current_path).to eq root_path
+      # ログアウトボタンがあることを確認
+      expect(page).to have_content('ログアウト')
+      # 新規登録ボタンや、ログインボタンが表示されていないことを確認
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
+    end
+  end
+
+  context '新規登録できないとき' do
+
+    it '誤った情報では新規登録できず新規登録ページにとどまる' do
+      # トップページに移動
+      visit root_path
+      # トップページに新規登録ボタンがあることを確認
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動
+      visit new_user_registration_path
+      # ユーザー情報を入力
+      fill_in 'user_nickname', with: ""
+      fill_in 'user_email', with: ""
+      fill_in 'user_password', with: ""
+      fill_in 'user_password_confirmation', with: ""
+      # 登録ボタンを押してもユーザーモデルのカウントは上がらないことを確認
+      expect{find('input[name="commit"]').click}.to change { User.count }.by(0)
+      # 新規登録ページへ戻されることを確認する
+      expect(current_path).to eq "/users" 
+    end
+  end
+end
